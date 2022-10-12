3713e77e5dd420ac443c685510baec77e337d1b0
Jared <jbeckersalax9@aol.com>
2022-10-12 12:55:52 -0400

    commit tree 30c21445673e2cb318554e20b5a7ecdc0a2cd24d

--- src/managePresents.js
+++ src/managePresents.js
@@ -2,4 +2,14 @@ const initialState = { numberOfPresents: 0 };
 
 export function managePresents(state = initialState, action) {
   // your code here
+  
+  // presents/increase to increase state by 1
+
+  switch(action.type){
+    case "presents/increase":
+      return { numberOfPresents: state.numberOfPresents + 1 };
+    default:
+      return state;
+  }
+
 }
